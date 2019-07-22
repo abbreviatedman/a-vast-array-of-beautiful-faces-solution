e8a056e55db3704a03db3a6d841bc45a93d23fe8
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-22 13:55:28 -0400

    initial commmit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Vast Array Of Beautiful Faces - Solution
+
+One possible set of solutions to [A Vast Array Of Beautiful Faces](https://github.com/abbreviatedman/a-vast-array-of-beautiful-faces)
---
+++ main.js
@@ -0,0 +1,56 @@
+const classroom = ['Colin', 'Mesuara'];
+
+console.log('# Challenge #1');
+
+// Manually add every student BUT yourself to the above, and console out how many are in there, using `length`.
+
+console.log('# Challenge #2');
+
+// Now use `push` to get yourself in there, and console out the new length.
+
+console.log('# Challenge #3');
+
+// Now console out the 1st person. It should be 'Colin'!
+
+console.log('# Challenge #4');
+
+// Console out the 5th person. Don't forget to start counting at 0!
+
+console.log('# Challenge #5');
+
+// Now console out the last, but use `length` to find it.
+
+console.log('# Challenge #6');
+
+// Now console out the next to last, still using `length`.
+
+console.log('# Challenge #7');
+
+// Remove the last person and console out the new length of the array.
+
+console.log('# Challenge #8');
+
+// Now console out the person you just removed. You might have to change the code from the previous # challenge to make this possible!
+
+console.log('# Challenge #9');
+
+// Now remove and console out the name of the _first_ person on the list.
+
+
+console.log('# Challenge #10');
+
+// Now remove and console out the name of the _new_ first person on the list.
+
+console.log('# Challenge #11');
+
+// Now add 'Ginny' to the _start_ of the list.
+
+console.log('# Challenge #11');
+
+// Now add 'Yuri' to the start of the list.
+
+console.log('# Challenge #12');
+
+// Now console out the whole list. You should only need one `console.log`!
+
+// It should be the entire list of students minus yourself, with 'Colin' and 'Mesuara' replaced with 'Yuri' and 'Ginny'. In other words, the list of people you'll be around in Term 2!
